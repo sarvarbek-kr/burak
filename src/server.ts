d88be587c1224a70beb9b51e7baf6303e4e69659
Walter <sarvarbek200028@gmail.com>
2024-09-06 17:45:26 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,11 @@
 // MVC = Model view controller = asosan backend da ishlatiladi.
 
 // Design pattern: Middleware, Decotar    == malum bir suyak (qo'l masalan)
+
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log("PORT:",process.env.PORT);
+
+console.log("MONGO_URL:",process.env.MONGO_URL);
