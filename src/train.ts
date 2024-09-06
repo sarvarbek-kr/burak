61b82c1cac9eb260954e8b6174f87fded93b6a01
Walter <sarvarbek200028@gmail.com>
2024-09-06 18:43:34 +0900

    feat: H task completed

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,9 @@
 console.log("Heelo train");
+
+
+function getPositive(arr: number[]): string {
+    return arr.filter(x => x > 0).join('');
+}
+
+// Funksiyani sinab ko'ramiz
+console.log(getPositive([1, -4, 2]));  // Natija: "12"
