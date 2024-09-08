bf2bc73af8d6eda726ea58cf2576f245c41aa782
Walter <sarvarbek200028@gmail.com>
2024-09-09 00:58:35 +0900

    fix: task moved js file to ts

--- src/train.ts
+++ src/train.ts
@@ -1,9 +1,22 @@
-console.log("Heelo train");
+// console.log("Heelo train");
 
 
-function getPositive(arr: number[]): string {
-    return arr.filter(x => x > 0).join('');
+// function getPositive(arr: number[]): string {
+//     return arr.filter(x => x > 0).join('');
+// }
+
+// // Funksiyani sinab ko'ramiz
+// console.log(getPositive([1, -4, 2]));  // Natija: "12"
+
+
+// H2-TASK: 
+
+// Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+// MASALAN: getDigits("m14i1t") return qiladi "141".
+
+
+function getDigits(str: string): string {
+    return str.replace(/\D/g, ''); // \D - digit bo'lmagan belgilarni o'chiradi
 }
 
-// Funksiyani sinab ko'ramiz
-console.log(getPositive([1, -4, 2]));  // Natija: "12"
+console.log(getDigits("m14i1t")); // Output: "141"
