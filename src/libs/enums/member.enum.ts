ef470e7202dbe52ce668e24c8065de291f00bf17
Walter <sarvarbek200028@gmail.com>
2024-09-08 21:42:55 +0900

    feat: develop member schema model

---
+++ src/libs/enums/member.enum.ts
@@ -0,0 +1,4 @@
+export enum MemberType {
+    USER = "USER",
+    RESTAURANT = "RESTAURANT",
+}
---
+++ src/schema/Member.model.ts
@@ -0,0 +1,51 @@
+import mongoose, {Schema} from "mongoose";
+import { MemberStatus, MemberType } from"../libs/enums/member.enum";
+
+//schema first orqali hosil qildik Code firs ham bor hozir emas.
+const memberSchema = new Schema({
+    memberType: {
+        type: String,
+        enum: MemberType,
+        default:MemberType.USER,
+    },
+    memberStatus: {
+        type: String,
+        enum: MemberStatus,
+        default: MemberStatus.ACTIVE,
+
+    },
+    
+    memberNick: {
+        type:String,
+        index: {unique: true, sparse: true},
+        required: true,
+    },
+    memberPhone: {
+        type: String,
+        index: { unique: true, sparse: true},
+        required:true
+    },
+
+    memberPassword: {
+        type: String,
+        select: false,
+        required:true,
+    },
+
+    memberAddress: {
+        type: String,
+    },
+    memberDesc: {
+        type: String,
+    },
+    memberImage: {
+        type: String,
+    },
+
+    memberPoint: {
+        type: Number,
+        default:0,
+    },
+}, 
+{timestamps: true} // qachon updatedAT, createdAd boganini aniqlaydi timesstamps
+);
