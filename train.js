3cb926aa43387f81f1844e31d103f9d837885f06
Walter <sarvarbek200028@gmail.com>
2024-09-08 21:57:54 +0900

    feat: h2 task completed

--- train.js
+++ train.js
@@ -5,13 +5,27 @@
 // MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 
 
-function getHighestIndex(arr) {
-    // Arraydagi eng katta qiymatni topish
-    const maxValue = Math.max(...arr);
-    // Eng katta qiymatning birinchi uchragan indeksini qaytarish
-    return arr.indexOf(maxValue);
+// function getHighestIndex(arr) {
+//     // Arraydagi eng katta qiymatni topish
+//     const maxValue = Math.max(...arr);
+//     // Eng katta qiymatning birinchi uchragan indeksini qaytarish
+//     return arr.indexOf(maxValue);
+// }
+
+// // Misol
+// const result = getHighestIndex([86,45,91,19]);
+// console.log(result);  // Natija: 1
+
+
+// H2-TASK: 
+
+// Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+// MASALAN: getDigits("m14i1t") return qiladi "141"
+
+
+
+function getDigits(str) {
+    return str.replace(/\D/g, ''); // \D - digit bo'lmagan barcha belgilarni o'chiradi
 }
 
-// Misol
-const result = getHighestIndex([86,45,91,19]);
-console.log(result);  // Natija: 1
+console.log(getDigits("m14i1t")); // Output: "141"
