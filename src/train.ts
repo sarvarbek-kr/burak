b61ad60ef3c24df613eb3b48d0c12cf6cb2df2c3
Walter <sarvarbek200028@gmail.com>
2024-10-11 18:24:03 +0900

    feat: task T done

--- src/train.ts
+++ src/train.ts
@@ -270,13 +270,27 @@ REQUESTLAR :
 // MASALAN: missingNumber([3, 0, 1]) return 2
 
 
-function missingNumber(nums: number[]): number {
-    const n = nums.length;
-    const expectedSum = (n * (n + 1)) / 2;
-    const actualSum = nums.reduce((acc, curr) => acc + curr, 0);
+// function missingNumber(nums: number[]): number {
+//     const n = nums.length;
+//     const expectedSum = (n * (n + 1)) / 2;
+//     const actualSum = nums.reduce((acc, curr) => acc + curr, 0);
     
-    return expectedSum - actualSum;
-}
+//     return expectedSum - actualSum;
+// }
 
-// 
-console.log(missingNumber([3, 0, 1])); 
+// // 
+// console.log(missingNumber([3, 0, 1])); 
+
+
+// T-TASK:
+
+// Shunday function yozing, u sonlardan tashkil topgan 2 ta array qabul qilsin va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
+// MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
+
+function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
+    return [...arr1, ...arr2].sort((a, b) => a - b);
+  }
+  
+  // Sinov
+  console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+  
