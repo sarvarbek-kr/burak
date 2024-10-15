b43cb867b0d7a6f0a59cbce508672e0bcf18ae7c
Walter <sarvarbek200028@gmail.com>
2024-10-16 00:02:32 +0900

    feat: task U done

--- src/train.ts
+++ src/train.ts
@@ -287,10 +287,28 @@ REQUESTLAR :
 // Shunday function yozing, u sonlardan tashkil topgan 2 ta array qabul qilsin va ikkala arraydagi sonlarni tartiblab bir arrayda qaytarsin
 // MASALAN: mergeSortedArrays([0,3,4,31], [4,6,30]); return [0,3,4,4,6,30,31]
 
-function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
-    return [...arr1, ...arr2].sort((a, b) => a - b);
+// function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
+//     return [...arr1, ...arr2].sort((a, b) => a - b);
+//   }
+  
+//   // Sinov
+//   console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+  
+
+// U-TASK:      11-October 2024
+
+// Shunday function yozing, uni number parametri bolsin va 0 dan berilgan parametrgacha bolgan oraliqdagi faqat toq sonlar nechtaligini return qilsin
+// MASALAN: sumOdds(9) return 4; sumOdds(11) return 5;
+
+function sumOdds(number: number): number {
+    let count = 0;
+    for (let i = 1; i <= number; i += 2) {
+      count++;
+    }
+    return count;
   }
   
-  // Sinov
-  console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+  console.log(sumOdds(9));  // 4
+  console.log(sumOdds(15)); // 8
   
+
